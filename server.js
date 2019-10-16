f08b4cb4b75edf5c2796898c9914cc2446bd425f
Stuart McKay <stuart51994@gmail.com>
2019-10-15 17:09:58 -0700

    add server path to allow client to req metrics and return all data using promises

--- server.js
+++ server.js
@@ -16,3 +16,28 @@ if (process.send.NODE_ENV === 'production') {
 server.listen(port, () => {
   console.log(`Server running at localhost:${port}`);
 });
+
+app.get('/api', (req, res) => {
+  const { metrics, startDate, endDate } = req.query;
+  console.log(`Requested metrics: ${metrics}`);
+  console.log(`Requested start-date: ${startDate}`);
+  console.log(`Requested end-date: ${endDate}`);
+  Promise.all(getData(metrics ? metrics.split(',') : metrics, startDate, endDate))
+    .then((data) => {
+      // flatten list of objects into one object
+      const body = {};
+      Object.values(data).forEach((value) => {
+        Object.keys(value).forEach((key) => {
+          body[key] = value[key];
+        });
+      });
+      res.send({ data: body });
+      console.log('Done');
+    })
+    .catch((err) => {
+      console.log('Error:');
+      console.log(err);
+      res.send({ status: 'Error getting a metric', message: `${err}` });
+      console.log('Done');
+    });
+});
