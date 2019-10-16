f6ebe4e2f01de00fd3d7175c84abdf84248e52fc
Stuart McKay <stuart51994@gmail.com>
2019-10-15 17:10:58 -0700

    add new path to dispaly fetched gAnalytics data with a graph

--- server.js
+++ server.js
@@ -41,3 +41,32 @@ app.get('/api', (req, res) => {
       console.log('Done');
     });
 });
+
+app.get('/api/graph', (req, res) => {
+  const { metric } = req.query;
+  console.log(`Requested graph of metric: ${metric}`);
+  // 1 week time frame
+  let promises = [];
+  for (let i = 7; i >= 0; i -= 1) {
+    promises.push(getData([metric], `${i}daysAgo`, `${i}daysAgo`));
+  }
+  promises = [].concat(...promises);
+  Promise.all(promises)
+    .then((data) => {
+      // flatten list of objects into one object
+      const body = {};
+      body[metric] = [];
+      Object.values(data).forEach((value) => {
+        body[metric].push(value[metric.startsWith('ga:') ? metric : `ga:${metric}`]);
+      });
+      console.log(body);
+      res.send({ data: body });
+      console.log('Done');
+    })
+    .catch((err) => {
+      console.log('Error:');
+      console.log(err);
+      res.send({ status: 'Error', message: `${err}` });
+      console.log('Done');
+    });
+});
